8352cac2d0cb5110578aa29e31381104f353f417
Hamed Karami <hkarami8@gmAIL.COM>
2022-04-05 20:12:13 +0430

    Implement User Login

---
+++ Models/Accounts/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentitySample.Models.Accounts
+{
+    public class LoginModel
+    {
+        #region [Properties]
+        [Required]
+        [Display(Name = "User Name:")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Password:")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me:")]
+        public bool RememberMe { get; set; }
+        #endregion
+    }
+}
